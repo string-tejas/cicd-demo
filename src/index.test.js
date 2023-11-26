c214790d4621ef4a99bc3bde4a84d0b9222184a0
Tejas <jtjtejas05@gmail.com>
2023-11-26 11:14:26 +0530

    Add calculator functions and update HTML layout

--- src/index.test.js
+++ src/index.test.js
@@ -1,20 +1,25 @@
 // import files
-var add = require('./calculator/add')
-var subtract = require('./calculator/subtract')
-var multiply = require('./calculator/multiply')
-var divide = require('./calculator/divide')
-
+var add = require("./calculator/add");
+var subtract = require("./calculator/subtract");
+var multiply = require("./calculator/multiply");
+var divide = require("./calculator/divide");
 
 // Test Case 1 (Addition)
 test("Should add two numbers", () => {
-  expect(add(25, 10)).toBe(35);
+    expect(add(25, 10)).toBe(35);
 });
 
 // Test Case 2 (Subtraction)
 test("Should subtract two numbers", () => {
-  expect(subtract(25, 10)).toBe(15);
+    expect(subtract(25, 10)).toBe(15);
 });
 
 // Test Case 3 (Multiplication)
+test("Should subtract two numbers", () => {
+    expect(multiply(25, 10)).toBe(25 * 10);
+});
 
 // Test Case 4 (Division)
+test("Should subtract two numbers", () => {
+    expect(divide(25, 10)).toBe(25 / 10);
+});
